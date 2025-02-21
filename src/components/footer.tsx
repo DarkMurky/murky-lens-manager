50e14384602594be39ecfd06fcaa1ea5820617c8
Karolis <karolisvaitiekunas@protonmail.com>
2025-02-21 17:29:04 +0200

    chore: update credits text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Footer() {
 	return (
 		<footer className="bg-primary text-primary-foreground flex flex-col justify-around items-center p-1 fixed left-0 bottom-0 w-full h-footer sm:items-start">
-			<h1 className="scroll-m-20 text-sm font-bold tracking-tight sm:pl-4">Made by: DarkMurky</h1>
+			<h1 className="scroll-m-20 text-sm font-bold tracking-tight sm:pl-4">Made by DarkMurky</h1>
 			<div>
 				<div className="flex h-5 items-center text-sm space-x-4 ">
 					<Link href="/about">
